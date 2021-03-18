a2e8110aea69e35491dc7868e58fe0adf64917ee
Aubrey Jones <ajones@flagshipbio.com>
2021-03-17 19:35:31 -0600

    More intelligent parsing, I guess.

--- plotting/hpgl.py
+++ plotting/hpgl.py
@@ -23,18 +23,25 @@ def reduce_coord(f, coords, start, index):
 def reduce_coords(f, coords, start):
     return (reduce_coord(f, coords, start, 0), reduce_coord(f, coords, start, 1))
 
+def coord_extents(coords):
+    maxes = reduce_coords(max, coords, (-2**32, -2**32))
+    mins = reduce_coords(min, coords, (2**32, 2**32))
+    return (mins, maxes)
+
+def strip_endline(l):
+    return l[:-2] if l.endswith(';\n') else l
 
 class Statement:
-    def __init__(self, line):
+    def __init__(self, line: str):
         self.command = line[:2]
-        self.tail = line[2:-2] # drop the ';\n'
+        self.tail = strip_endline(line[2:])
         self.split_tail = self.tail.split(',') if self.tail else []
         self.parsed_args = []
         self._parse_args()
     
     def _parse_args(self):
         if not self.split_tail: return
-        if self.command in ('PU', 'PD', 'PA'):
+        if self.is_trace():
             self.parsed_args = parse_list_as_coords(self.split_tail)
         elif self.command in ('SP'):
             self.parsed_args = parse_list_as_type(self.split_tail, int)
@@ -48,6 +55,13 @@ class Statement:
     
     def uncuttable(self):
         return self.command in ('LB', 'DT', 'DI')
+
+    def is_trace(self):
+        return self.command in ('PU', 'PD', 'PA')
+
+    def set_args(self, *args):
+        self.parsed_args = list(args)
+        self.rewrite()
 
     def rewrite(self):
         if not self.parsed_args: return
@@ -64,6 +78,9 @@ class Block:
     
     def __repr__(self):
         return '***\n' + "\n".join(map(lambda s: "\t" + repr(s), self.commands))
+    
+    def __str__(self):
+        return "".join(map(str, self.commands))
 
     def __iter__(self):
         return iter(self.commands)
@@ -73,7 +90,12 @@ class Block:
     
     def cuttable(self):
         return not self.uncuttable()
-
+    
+    def has_trace(self):
+        return any(map(Statement.is_trace, self.commands))
+    
+    def extents(self):
+        return coord_extents(list(chain(*map(lambda s: s.parsed_args, filter(Statement.is_trace, self.commands)))))
 
 class HPGLPlot:
     def __init__(self):
@@ -98,13 +120,24 @@ class HPGLPlot:
         return chain(*self.blocks)
 
     def __repr__(self):
-        return "\n".join(map(lambda s: repr(s), iter(self)))
+        return "\n".join(map(repr, iter(self)))
+    
+    def __str__(self):
+        return "".join(map(str, iter(self)))
     
     def no_cuttable(self):
         return filter(Block.cuttable, self)
 
     def cuttable_repr(self):
         return "\n".join(map(lambda s: repr(s), self.no_cuttable()))
+    
+    def extents(self):
+        return coord_extents(list(chain(*map(Block.extents, filter(Block.has_trace, self.blocks)))))
+
+    def mirror(self):
+        bounds = self.extents()
+        self.init_statements['IP'].set_args(0, bounds[1][1], bounds[1][0], 0)
+        self.init_statements['SC'].set_args(0, 1, 0, -1, 2)
 
 
 def parse_lines(lines):
@@ -118,3 +151,7 @@ def parse_lines(lines):
         plot.push_statement(statement)
     
     return plot
+
+def parse_file(filename):
+    with open(filename) as f:
+        return parse_lines(f.readlines())

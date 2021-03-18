034e0b70737eee543b1088920ccd0cbf11fcb11e
Aubrey Jones <ajones@flagshipbio.com>
2021-03-18 15:13:41 -0600

    Working on interactive tool to mark cuts.

--- plotting/hpgl.py
+++ plotting/hpgl.py
@@ -7,6 +7,7 @@ from functools import reduce
 import subprocess, io
 
 from PIL import Image, ImagePalette
+import shapely, shapely.geometry
 
 def iterate_as_type(strings, t):
     return map(t, strings)
@@ -25,6 +26,13 @@ def reduce_coord(f, coords, start, index):
 
 def reduce_coords(f, coords, start):
     return (reduce_coord(f, coords, start, 0), reduce_coord(f, coords, start, 1))
+
+def flatten_coords(coords):
+    retval = []
+    for x, y in coords:
+        retval.append(x)
+        retval.append(y)
+    return retval
 
 def coord_extents(coords):
     maxes = reduce_coords(max, coords, (-2**32, -2**32))
@@ -48,7 +56,7 @@ class Statement:
     
     def _parse_args(self):
         if not self.split_tail: return
-        if self.is_trace():
+        if self.needs_coordinates():
             self.parsed_args = parse_list_as_coords(self.split_tail)
         elif self.command in ('SP'):
             self.parsed_args = parse_list_as_type(self.split_tail, int)
@@ -63,8 +71,11 @@ class Statement:
     def uncuttable(self):
         return self.command in ('LB', 'DT', 'DI')
 
-    def is_trace(self):
+    def needs_coordinates(self):
         return self.command in ('PU', 'PD', 'PA')
+
+    def is_trace(self):
+        return self.command in ('PD')
 
     def set_args(self, *args):
         self.parsed_args = list(args)
@@ -100,9 +111,41 @@ class Block:
     
     def has_trace(self):
         return any(map(Statement.is_trace, self.commands))
+
+    def has_coordinates(self):
+        return any(map(Statement.needs_coordinates, self.commands))
     
     def extents(self):
-        return coord_extents(list(chain(*map(lambda s: s.parsed_args, filter(Statement.is_trace, self.commands)))))
+        return coord_extents(list(chain(*map(lambda s: s.parsed_args, filter(Statement.needs_coordinates, self.commands)))))
+
+    def get_pen(self):
+        for s in self:
+            if s.command == 'SP':
+                return s.parsed_args[0]
+        return None
+    
+    def set_pen(self, pen_number):
+        for s in self:
+            if s.command == 'SP':
+                s.set_args(pen_number)
+                break
+
+    def trace(self):
+        if not self.has_trace(): return None
+        block_trace = []
+        for cmd in self:
+            if cmd.command == 'PU' and cmd.parsed_args:
+                block_trace.append(cmd.parsed_args[0])
+            if cmd.command == 'PD':
+                block_trace.extend(cmd.parsed_args)
+        return block_trace
+
+    def distance_to_trace(self, point: tuple):
+        if not self.has_trace(): return 2**31
+        query_point = shapely.geometry.Point(*point)
+        trace_string = shapely.geometry.LineString(self.trace())
+        distance = trace_string.distance(query_point)
+        return distance
 
 class HPGLPlot:
     def __init__(self):
@@ -112,7 +155,7 @@ class HPGLPlot:
     def push_block(self):
         self.blocks.append(Block())
     
-    def push_statement(self, statement):
+    def push_statement(self, statement: Statement):
         if statement.command in ('IP', 'SC'):
             self.init_statements[statement.command] = statement
         self.last_block().push_back(statement)
@@ -139,7 +182,7 @@ class HPGLPlot:
         return "\n".join(map(lambda s: repr(s), self.cuttable()))
     
     def extents(self):
-        return coord_extents(list(chain(*map(Block.extents, filter(Block.has_trace, self.blocks)))))
+        return coord_extents(list(chain(*map(Block.extents, filter(Block.has_coordinates, self.blocks)))))
 
     def mirror(self):
         bounds = self.extents()
